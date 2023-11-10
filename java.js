5d06f042cae692e91fdf128cadb8a58778fadc63
davidetodesco <davideetodesco@gmail.com>
2023-11-10 19:25:16 +0100

    Create etch-a-sketch, start creating eraser button

--- java.js
+++ java.js
@@ -1,9 +1,35 @@
 const container = document.querySelector('.container');
 
 for (let i = 0; i < (16 * 16); i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  square.style.width = '37.5px'
-  square.style.height = '37.5px'
-  container.appendChild(square);
+  const squares = document.createElement('div');
+  squares.classList.add('square');
+  squares.style.width = '37.5px';
+  squares.style.height = '37.5px';
+  container.appendChild(squares);
 }
+
+function getRandom () {
+  Math.floor((Math.random() * 255)+1)
+}
+
+function getNewBg() {
+  let a = getRandom()
+  let b = getRandom()
+  let c = getRandom()
+  squares.style.backgroundColor = (a, b, c)
+}
+
+let singleSquares = document.querySelectorAll('.square') 
+
+singleSquares.forEach((square) => {
+  const singSquare = square;
+  singSquare.addEventListener('mouseenter',(x) =>{
+    x.target.style.backgroundColor = 'black'
+  } )
+})
+
+const button = document.querySelector('.erase')
+
+button.addEventListener('click', () => {
+  squares.target.style.backgroundColor = 'white'
+} )
